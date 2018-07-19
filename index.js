7d2d5d0a9be274fb657ef64c05c4593925882daa
AspenJames <aspenjames@tqca.org>
2018-07-18 21:59:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const app = "I don't do much.";
+const token = "aec214042811cf7085d0961cc3382c3bebb90514";
+fetch("https://api.github.com/user/repos", {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
